66a04dca195b32a67ffeb768da34f457f273140c
Ayush73nigam <redemptstalin23@gmail.com>
2023-04-04 13:38:33 +0530

    debug1_#4

--- debug1.cpp
+++ debug1.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int count_vowels(string str) {
+
     int count = 0;
-    for (int i = 0; i < str.length(); i++) {
+    for (int i = 0; str[i]!='\0' || (str[i]==' ' && str[i+1]=='\0'); i++) {
         char c = tolower(str[i]);
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
             count++;
@@ -15,9 +16,17 @@ int count_vowels(string str) {
 }
 
 int main() {
-    string input;
+    char input[100];
     cout << "Enter a string: ";
-    cin>>input;
+    fgets(input,100,stdin);  
+
+    int counter = 0;
+    for (int i = 0; input[i]; i++){     
+        if (input[i] != ' ')
+            input[counter++] = input[i]; 
+    }
+    input[counter] = '\0';       
+
     cout << "The number of vowels in the string is: " << count_vowels(input) << endl;
     return 0;
 }
