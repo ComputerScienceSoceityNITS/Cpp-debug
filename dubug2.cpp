3bf1a0356131c35fa0c974503a71a220de3e4968
INDRAJIT DAS <indrajiitdas057@gmail.com>
2023-04-04 13:39:23 +0530

    fixed debug2

--- dubug2.cpp
+++ dubug2.cpp
@@ -6,8 +6,14 @@ bool is_prime(int num) {
     if (num <= 1) {
         return false;
     }
-    for (int i = 2; i * i <= num; i++) {
-        if (num % i == 0) {
+    if(num <= 3){
+        return true;
+    }
+    if(num % 2 == 0 || num % 3 == 0){
+        return false;
+    }
+    for (int i = 5; i * i <= num; i+= 6) {
+        if (num % i == 0 || num % (i + 2) == 0) {
             return false;
         }
     }
