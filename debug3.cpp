82d33bbb6dd71e932143526461eeb7cf1f026ad4
bits-and-atoms <majumdersubham14082003@gmail.com>
2023-04-08 01:11:23 +0530

    #1_issue_solved_debug3.cpp

--- debug3.cpp
+++ debug3.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int second_largest_number(vector<int>& nums) {
     int n = nums.size();
-    int largest = -1e5;
-    int second_largest = -1e5;
+    int largest = (n >= 1) ? nums[0] : -1e5;
+    int second_largest = (n >= 2) ? nums[1] : -1e5;
     for (int i = 0; i < n; i++) {
         if (nums[i] > largest) {
             second_largest = largest;
