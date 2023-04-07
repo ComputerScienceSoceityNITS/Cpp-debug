3b27f676d4d3d9dd8181a48c9eb3c6d99842dced
bits-and-atoms <majumdersubham14082003@gmail.com>
2023-04-08 01:48:31 +0530

    #5_issue_solved_recursioncreation.cpp

--- recursioncreation.cpp
+++ recursioncreation.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 
 //create a recursive funtion of the following logic given in the main function.
-
+int factorial(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n-1);
+    }
+}
 int main(){
     int n ;
     cout<<"Enter the number : ";
