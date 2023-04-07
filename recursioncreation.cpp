6cb08f0ad6e0147387dc857d808b9f068cff8fd9
Ayush73nigam <redemptstalin23@gmail.com>
2023-04-07 16:04:45 +0530

    recursioncreation_#5 done

--- recursioncreation.cpp
+++ recursioncreation.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 
-//create a recursive funtion of the following logic given in the main function.
+int recursion(int n){
+    if(n==1){
+        return 1;
+    }
+    int fact=recursion(n-1)*n;
+    return fact;
+}
 
 int main(){
     int n ;
     cout<<"Enter the number : ";
     cin>>n;
-    int ans = 1; 
-    for(int i = n ; i  >= 1 ;i--){
-        ans = ans * i;   
-    }
-    cout<<ans<<endl;
+    cout<<recursion(n)<<endl;
 }
