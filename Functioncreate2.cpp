f9f6c96907e332583e68b1b9e199d6c3343ec197
bits-and-atoms <majumdersubham14082003@gmail.com>
2023-04-08 00:57:33 +0530

    #22_issue_solved_Functioncreate2

--- Functioncreate2.cpp
+++ Functioncreate2.cpp
@@ -4,6 +4,16 @@ using namespace std;
 string insert_space(string text)
 {
     // write the function code here
+    string result;
+    for (int i = 0; i < text.length(); i++)
+    {
+        if (i != 0 && text[i] >= 'A' && text[i] <= 'Z')
+        {
+            result += ' ';
+        }
+        result += text[i];
+    }
+    return result;
 }
 
 int main()
