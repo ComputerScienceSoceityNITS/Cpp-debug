b4c8147c40ce14d32a1dce4ca1f180530842592f
Ayush73nigam <redemptstalin23@gmail.com>
2023-04-07 13:56:29 +0530

    debug4_#23_correct

--- debug4.cpp
+++ debug4.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-int q(int arr[], int l, int r, int x)
+int q(int arr[], int x, int n)
 {
-    if (r >= l)
-    {
-        int o = l + (r - l) / 2;
-
-        if (arr[o] == x)
-            return o;
-
-        if (arr[o] > x)
-            return q(arr, l, o - 1, x);
-
-        return q(arr, o + 1, r, x);
+    for(int i=0;i<n;i++){
+        if(x==arr[i]){
+            return i;
+        }
     }
-
     return -1;
 }
 
@@ -24,7 +16,7 @@ int main(void)
     int arr[] = {2, 3, 4, 10, 40};
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
-    int result = q(arr, 0, n - 1, x);
+    int result = q(arr, x, n);
     (result == -1)
         ? cout << "Element is not present in array"
         : cout << "Element is present at index " << result;
