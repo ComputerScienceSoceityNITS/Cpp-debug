5f9daf629c53b60dea6a033e62eca006924e3dae
INDRAJIT DAS <indrajiitdas057@gmail.com>
2023-04-08 22:00:30 +0530

    functioncreate2 fixed

--- Functioncreate2.cpp
+++ Functioncreate2.cpp
@@ -4,6 +4,15 @@ using namespace std;
 string insert_space(string text)
 {
     // write the function code here
+    if(text.length() <= 1){
+        return text;//it is the base case where all the recursive function depends
+    }else{
+        string firstchar =  text.substr(0,1);
+        // here text.substr function in cpp extract a substring from the text string
+        //starting from the first character ie positon 0 and extracting 1 character.
+        string remaingchar = text.substr(1);
+        return firstchar + " " + insert_space(remaingchar);
+    }
 }
 
 int main()
