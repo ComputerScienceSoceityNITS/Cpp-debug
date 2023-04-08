b9e6ee57aa3f96b188949e3c183270b4d90b59c6
prince7703 <princekoshti7703@gmail.com>
2023-04-08 12:44:01 +0530

    modified check function

--- logicalproblem02.cpp
+++ logicalproblem02.cpp
@@ -4,6 +4,15 @@ using namespace std;
 bool check(char a, char b)
 {
     // write the function here
+   
+    int ascii1=a-'a';
+    int ascii2=b-'b';
+    if((ascii1^ascii2 )== 0){
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
 int main()
