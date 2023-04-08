653ee0c4856fb021a4d8463c609326ba0469f4d7
prince7703 <princekoshti7703@gmail.com>
2023-04-08 12:55:54 +0530

    added iterative function

--- debug4.cpp
+++ debug4.cpp
@@ -3,30 +3,50 @@ using namespace std;
 
 int q(int arr[], int l, int r, int x)
 {
-    if (r >= l) // for ensuring the right and left pointer position
+    if (r >= l)
     {
-        int o = l + (r - l) / 2; //calculating the mid
+        int o = l + (r - l) / 2;
 
-        if (arr[o] == x)// if matches with target return its index
+        if (arr[o] == x)
             return o;
 
-        if (arr[o] > x)//if greater ,search in left side 
+        if (arr[o] > x)
             return q(arr, l, o - 1, x);
 
-        return q(arr, o + 1, r, x); //else search in right side 
+        return q(arr, o + 1, r, x);
     }
 
-    return -1;//if not found!! return -1
+    return -1;
+}
+int binarysearch(int arr[],int x){
+    int s=0;
+    int e=arr.size()-1;
+    int mid;
+    while(s<=e){
+      mid=s+(e-s)/2;
+        
+        if(arr[mid]==x){
+            return mid;
+        }
+
+       else if(arr[mid]<x){
+            s=mid+1;
+        }
+        else {
+            e=mid-1;
+        }
+    }
+    return -1;
 }
 
 int main(void)
 {
     int arr[] = {2, 3, 4, 10, 40};
-    int x = 10;           // target element 
-    int n = sizeof(arr) / sizeof(arr[0]);//calculating the size of the array
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
     int result = q(arr, 0, n - 1, x);
-    (result == -1) 
-        ? cout << "Element is not present in array"//if result ==-1 then this condition will get executed
-        : cout << "Element is present at index " << result; //else this 
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
     return 0;
 }
