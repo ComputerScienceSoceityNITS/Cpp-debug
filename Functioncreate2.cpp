94822d41b72ab1cbc65abb54862c6f705560da93
prince7703 <princekoshti7703@gmail.com>
2023-04-08 11:31:50 +0530

    modified insert_space funtion

--- Functioncreate2.cpp
+++ Functioncreate2.cpp
@@ -4,6 +4,18 @@ using namespace std;
 string insert_space(string text)
 {
     // write the function code here
+    string s="";
+    s.push_back(text[0]);
+    for(int i=1;i<text.length();i++){
+        if(text[i]>='A' && text[i]<='Z'){
+            s.push_back(' ');
+            s.push_back(text[i]);
+        }
+        else {
+            s.push_back(text[i]);
+        }
+    }
+    return s;
 }
 
 int main()
@@ -11,6 +23,7 @@ int main()
     string text = "HelloWorldHelloWorldHelloWorldHelloWorld";
     cout << "Original String:\n";
     cout << text;
+    cout<<endl;
     cout << "Modified string :\n";
     cout << insert_space(text) << endl;
 }
