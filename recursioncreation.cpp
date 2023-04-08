8528a1890a32d13923d7ebf6773cefccdd9d40fa
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2023-04-08 18:31:11 +0530

    Fixed #5

--- recursioncreation.cpp
+++ recursioncreation.cpp
@@ -4,13 +4,19 @@ using namespace std;
 
 //create a recursive funtion of the following logic given in the main function.
 
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n-1);
+    }
+}
+
 int main(){
     int n ;
     cout<<"Enter the number : ";
     cin>>n;
-    int ans = 1; 
-    for(int i = n ; i  >= 1 ;i--){
-        ans = ans * i;   
-    }
-    cout<<ans<<endl;
+    
+    cout<<factorial(n)<<endl;
 }
