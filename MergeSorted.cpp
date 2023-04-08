f9fc2df293fd4dc838ed8b23651f757e784b2529
prince7703 <princekoshti7703@gmail.com>
2023-04-08 11:59:23 +0530

    completed merge_sorted function

--- MergeSorted.cpp
+++ MergeSorted.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+using namespace std;
 void print_vec(const std::vector<int> &vec)
 {
     std::cout << "Vector: ";
@@ -12,6 +13,32 @@ void print_vec(const std::vector<int> &vec)
 std::vector<int> merge_sorted(const std::vector<int> &vec1, const std::vector<int> &vec2)
 {
     //write the function here
+    int length1=vec1.size();
+    int length2=vec2.size();
+    vector<int>ans;
+      int i=0,j=0;
+    while(i<length1 && j<length2){
+        if(vec1[i]<vec2[j]){
+            ans.push_back(vec1[i]);
+            i++;
+        }
+        else {
+             ans.push_back(vec2[j]);
+            j++;
+        }
+    }
+
+    while(i<length1){
+         ans.push_back(vec1[i]);
+            i++;
+    }
+
+    while(j<length2){
+         ans.push_back(vec2[j]);
+            j++; 
+    }
+
+    return ans;
 }
 
 int main()
@@ -19,5 +46,7 @@ int main()
     std::vector<int> vec1 = {1, 3, 5, 7, 9};
     std::vector<int> vec2 = {2, 4, 6, 8, 10};
     std::vector<int> merged = merge_sorted(vec1, vec2);
+
+    print_vec(merged);
     return 0;
 }
